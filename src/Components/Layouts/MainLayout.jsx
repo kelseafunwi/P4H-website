cf39a27522bf12130e86f04858bff1724d911187
Funwi Kelsea <funwikelseandohnwi@gmail.com>
2024-02-25 00:58:05 +0100

    Fixed the issue with scroll restoration

--- src/Components/Layouts/MainLayout.jsx
+++ src/Components/Layouts/MainLayout.jsx
@@ -2,13 +2,20 @@ import Navbar from "../Navbar/Navbar.jsx";
 import {Outlet} from "react-router-dom";
 import {useEffect} from "react";
 import Footer from "../Footer.jsx";
+import {useLocation} from "react-router-dom";
 
 export const MainLayout = () => {
 
+    const {pathname} = useLocation();
 
     useEffect(() => {
         document.title = "Pro4Home website"
     }, [])
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+
+    }, [pathname]);
 
     return (
         <>
